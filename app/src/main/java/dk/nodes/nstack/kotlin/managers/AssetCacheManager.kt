fa30dfcc2fd5acd65151bfc47a505f96d1816b9b
Deniz Tunç Tuncer <dtunctuncer@gmail.com>
2019-07-23 12:44:00 +0200

    adds default language selection from assets

--- app/src/main/java/dk/nodes/nstack/kotlin/managers/AssetCacheManager.kt
+++ app/src/main/java/dk/nodes/nstack/kotlin/managers/AssetCacheManager.kt
@@ -1,8 +1,10 @@
 package dk.nodes.nstack.kotlin.managers
 
 import android.content.Context
+import dk.nodes.nstack.kotlin.NStack
 import dk.nodes.nstack.kotlin.util.asJsonObject
 import org.json.JSONObject
+import java.io.IOException
 import java.util.*
 
 
@@ -18,7 +20,16 @@ class AssetCacheManager(private val context: Context) {
             ?.toMap() ?: mapOf()
     }
 
+    private fun getDefaultTranslation(): String {
+        return try {
+            context.resources.assets.open("defaultLanguage.txt").bufferedReader().use { it.readText() }
+        } catch (e: IOException) {
+            ""
+        }
+    }
+
     private fun loadTranslation(translationFile: String): Translation? {
+        val defaultFileName = getDefaultTranslation()
         val pattern = "^translations_(\\d+)_(\\w{2}[-_]\\w{2})\\.json$".toRegex()
         val result = pattern.find(translationFile) ?: return null
         val groups = result.groupValues
@@ -29,6 +40,10 @@ class AssetCacheManager(private val context: Context) {
             inputStream.bufferedReader().use { it.readText() }.asJsonObject
         } catch (e: Exception) {
             null
+        }
+        // setting up default language for fallback
+        if (defaultFileName == translationFile) {
+            NStack.defaultLanguage = locale
         }
         return Translation(index = index, locale = locale, translations = translations)
     }
